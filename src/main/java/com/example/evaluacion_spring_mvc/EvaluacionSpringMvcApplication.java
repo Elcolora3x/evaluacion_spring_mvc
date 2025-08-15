27ddde224027e45804d8c6a94172d786213f2066
Elcolora3x <elcolora2x@gmail.com>
2025-08-15 17:29:50 -0300

    proyect_setup commit inicial primera evaluacion

---
+++ src/main/java/com/example/evaluacion_spring_mvc/EvaluacionSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.evaluacion_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/evaluacion_spring_mvc/EvaluacionSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evaluacion_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
